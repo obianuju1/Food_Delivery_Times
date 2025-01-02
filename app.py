3ddd2bd22a3a68e18ff6230959b32e3e4b143a7b
Obianuju <enekebe123@gmail.com>
2025-01-02 13:29:43 -0500

    the web app si ready

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def predict_api():
     new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
     output = regmodel.predict(new_data)
     return jsonify(output[0])
-""" 
+
 # Prediction form route
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -29,6 +29,6 @@ def predict():
     final_input = scalar.transform(np.array(data).reshape(1, -1))
     prediction = regmodel.predict(final_input)[0]
     return render_template('home.html', prediction_text=f"Predicted Delivery Time: {prediction:.2f} minutes")
- """
+
 if __name__ == "__main__":
     app.run(debug=True)
